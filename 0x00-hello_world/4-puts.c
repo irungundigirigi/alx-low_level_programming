0e7c41311087b1d4a197b3c4a7bb429822bcc9e4
irungundigirigi <irungundigirigi@gmail.com>
2023-03-16 19:49:00 +0300

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,17 @@
+/* 
+ * <stdio.h> imports the header file, part of the standard library
+ * which provides input and output functionality to the program
+ * */
+
+
 #include <stdio.h>
 
-int main (void) {
-	puts("Programming is like building a multilingual puzzle");
+int main(void) {
+	/*
+	 * Main - entry point of the program
+	 * Prints the content to standard output when invoked.
+	 * Return value
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
