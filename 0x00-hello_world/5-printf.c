be4a7bab7fdb3bca62ae5de4e202e9339f69afe2
irungundigirigi <irungundigirigi@gmail.com>
2023-03-17 00:35:30 +0300

    Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,6 @@
  * return value - 0 for success / any non-zero value means an error occured.
  * Description - program prints arguments passed printf to the standard output.
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
