d1ee22ec670092da3d2681a32d98ac382b59dc33
irungundigirigi <irungundigirigi@gmail.com>
2023-03-17 00:17:59 +0300

    Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/*
+ * main - programs entry-point
+ * return value - 0 for success / any non-zero value means an error occured
+ */
+
 int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
