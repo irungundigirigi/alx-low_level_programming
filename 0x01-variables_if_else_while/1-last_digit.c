c40cfaf00bd21344e46cd07f62d53ab807e47fc3
irungundigirigi <irungundigirigi@gmail.com>
2023-03-17 19:03:24 +0300

    Update

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
+ * Return: Always success 0
+ */
+
+
+int main(void)
+{
+	int n;
+	int mod_n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	mod_n = n % 10;
+	
+	if (mod_n > 5)
+
+		printf("The last digital of %d is %d and is greater than 5\n",n, mod_n);
+
+	else if ((mod_n < 6) && (mod_n != 0))
+
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",n,mod_n);
+	else
+
+		printf("The last digit of %d is %d and is 0\n",n, mod_n);
+	}
+	return (0);
+}
+
