df8aab3a66fffd58bd21666f86e1eb4e0ac14aaf
irungundigirigi <irungundigirigi@gmail.com>
2023-03-17 03:08:45 +0300

    Update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
 int main(void)
 {
 
-	char quote[ ]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quote[ ] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fprintf(stderr ,"%s" ,quote);
+	fprintf(stderr,"%s",quote);
 	return (1);
 }
