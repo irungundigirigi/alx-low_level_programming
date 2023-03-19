b084a36a10c5e456c214ac357108e4947cc5bbee
irungundigirigi <irungundigirigi@gmail.com>
2023-03-19 18:33:11 +0300

    Update README.md

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0.
- *
+ * main - prints all the numbers of base 16 in lowercase, followed by a new line. *
  * Return: Always 0.
  */
 int main(void)
@@ -11,6 +10,11 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 		putchar(i + '0');
+	/**
+	 * To print a digit with putchar convert digital to ASCII value
+	 * by using ASCII value of 0
+	 */
+
 	for (i = 'a'; i < 'g'; i++)
 		putchar(i);
 	putchar('\n');
