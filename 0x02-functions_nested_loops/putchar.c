c0a97c87c6f9bf5994704ee2b48a68f93f539b95
irungundigirigi <irungundigirigi@gmail.com>
2023-03-21 21:22:58 +0300

    Update

--- 0x02-functions_nested_loops/putchar.c
+++ 0x02-functions_nested_loops/putchar.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('h');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('h');
+	_putchar('\n');
 
 	return (0);
 }
