b4d519eaa5547b31412fe7b14b78f2990a955e6f
irungundigirigi <irungundigirigi@gmail.com>
2023-03-21 04:04:08 +0300

    Update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,12 +6,12 @@
  * Return: Always 0.
  */
 int main(void)
-{	
+{
 	int i,j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j < 10; j++ )
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
@@ -20,7 +20,7 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
-	}	
+	}
 	putchar('\n');
 
 	return (0);
