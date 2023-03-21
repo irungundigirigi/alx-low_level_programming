2f177648dc9802dcd575622ee5d5471fcae5e9ea
irungundigirigi <irungundigirigi@gmail.com>
2023-03-21 03:46:03 +0300

    Update task 9-print_comb.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,11 +6,13 @@
  * Return: Always 0.
  */
 int main(void)
-{	int i;
+{	
+	int i;
 	int j;
 
 	for (i = 0; i < 9; i++)
-	{	for (j = i + 1; j < 9; j++ )
+	{
+		for (j = i + 1; j < 9; j++ )
 		{
 			putchar('0' + i);
 			putchar('0' + j);
