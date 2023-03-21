3d6303b2a54429b8d6524e8b92e7477a279febca
irungundigirigi <irungundigirigi@gmail.com>
2023-03-21 23:49:58 +0300

    Update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	_putchar(letter);
+	putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,8 @@
  * Desc: Contains declarations for all functions used in directory 0x02-functions
  * _nested_loops
  */
-
 int _putchar(char c);
+void print_alphabet(void);
+
 
 #endif
