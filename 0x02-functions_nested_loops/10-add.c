43ef04a917f5773f03b2020042d12e54cbf5c969
irungundigirigi <irungundigirigi@gmail.com>
2023-03-22 17:04:11 +0300

    Update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add - Function name
+  * Description: Returns the value of two numbers.
+  * @a: the first parameter to be added.
+  * @b: the second parameter of type int
+  * Return: result of addition
+  */
+
+int add(int a, int b)
+{
+	return(sum = a + b);
+}
+
