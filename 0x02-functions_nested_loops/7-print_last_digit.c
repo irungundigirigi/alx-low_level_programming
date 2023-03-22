27f0d12b71f9c7896ed3568cdf757dc11b498641
irungundigirigi <irungundigirigi@gmail.com>
2023-03-22 12:45:00 +0300

    Update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-  * print_sign - Function name
-  * Description: Prints the sign of a number.
+  * print_sign - prints the sign of a number.
   * @n: parameter passed to the function for checking.
   * Return: value of last digit
   */
@@ -11,6 +10,11 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = n % 10;
+
+	if(last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+
 	return(last_digit);
 }
 
