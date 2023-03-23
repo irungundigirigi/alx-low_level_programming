14540c891ee07f5a55be118e18beeccbbdad032d
irungundigirigi <irungundigirigi@gmail.com>
2023-03-23 05:58:09 +0300

    Update

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * fibonachi
+  * @n: parameter passed to the function for checking.
+  * Return: value of last digit
+  */
+int fibonachi(int n)
+{
+
+	int sum;
+
+	for (int i = 1; i < n; i++)
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{	
+			sum += i;
+		}	
+
+}
+int main(void) 
+{
+	fibonachi(int n);
+}	
