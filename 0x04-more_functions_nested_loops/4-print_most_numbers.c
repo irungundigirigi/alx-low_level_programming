910b4f6521a2c40dad82c105b83477207523fdd2
irungundigirigi <irungundigirigi@gmail.com>
2023-03-23 17:42:34 +0300

    Update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,9 +5,12 @@
  * Return: return the a * b
  */
 
-
-for (i = 0; i < 9; i++)
+void print_most_numbers(void)
 {
-	if (i != 2 && i != 4)
-		putchar(i + '0');
+	for (i = 0; i <= 9; i++)
+
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+		else
+			continue
 }
