d3e085464ef61aaecd15ed8fcfd9bd2a92f3d046
irungundigirigi <irungundigirigi@gmail.com>
2023-03-23 23:19:55 +0300

    Update task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,6 +2,8 @@
 
 /**
  * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ * Description: the function loops through the numbers 0-9 and prints
+ * the numbers that meet the condition defined.
  */
 void print_most_numbers(void)
 {
