2a64449a15c11838e61b46f6933187f620704dd6
irungundigirigi <irungundigirigi@gmail.com>
2023-03-23 17:28:19 +0300

    Update task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,15 @@
 
 /**
  * _isupper - Checks if the letter passed as argument is uppercase.
+ * Description: Checks the ASCII value of c to determine its case.
  * @c:number to the checked
  * Return: 1 if uppercase and 0 if lowercase
  */
  
-int _isupper(int c) 
-{	
+int _isupper(int c)
+{
 	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
-}	
+}
