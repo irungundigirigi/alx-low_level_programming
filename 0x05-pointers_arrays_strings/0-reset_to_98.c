ff0ddcd47c83af9ffe97d1282cffbbb81194a625
irungundigirigi <irungundigirigi@gmail.com>
2023-03-28 00:12:10 +0300

    Update task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /*
- * reset_to_98 - takes a pointer to an int as parameter and updates value
- * @*n: takes a pointer as argument
+ * reset_to_98 - takes a pointer to an int as parameter and updates value.
+ * @n: takes a pointer as argument
  * Return: returns no value
  */
 
