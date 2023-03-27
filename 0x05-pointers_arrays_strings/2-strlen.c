d370d151444c9168b7c67202dd0b47f9c01906cd
irungundigirigi <irungundigirigi@gmail.com>
2023-03-28 00:26:21 +0300

    Update task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Find length of string. Iterates over the characters from i = 0
+ * until '\0' null character is encountered
+ * s: takes a string argument
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+		return (i);
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,7 @@
  */
 
 void reset_to_98(int *n);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
 
 #endif
