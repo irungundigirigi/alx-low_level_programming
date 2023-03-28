709acdee2c51c7dbd1de0d65e4df5a6078814048
irungundigirigi <irungundigirigi@gmail.com>
2023-03-28 04:53:50 +0300

    Update task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print every other char of a string
+ * @str: char array string type
+ * Description: loop over the string while printing on the char at even indexes
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
