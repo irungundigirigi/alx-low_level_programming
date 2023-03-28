cab5afdd3ef577cd51880ed378570ea752374c0e
irungundigirigi <irungundigirigi@gmail.com>
2023-03-28 05:46:34 +0300

    Update task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,10 @@
  */
 
 
-char *_strcpy(char *dest, char *srcy)
+char *_strcpy(char *dest, char *src)
 {
 	for(i = 0; src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-	}
 	dest[i] = '\0';
 
 	return (dest);
