4d81411b70ca92c7abef42e08143f99abdc1cea7
irungundigirigi <irungundigirigi@gmail.com>
2023-03-30 21:42:31 +0300

    Update task

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * reverse_array- reverse the contents of an array of int
@@ -9,11 +10,13 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+
 	int h;
 
 	for (i = 0; i < (n / 2); i++)
-	{
+	{	/* store the current index in variable h */
 		h = a[i];
+		/* Switch the first and last non-switched indexs */
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = h;
 	}
