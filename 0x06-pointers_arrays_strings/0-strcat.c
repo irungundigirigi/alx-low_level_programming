e4fdec8d38916ddfc2c226b1b4bc5a0995118c44
irungundigirigi <irungundigirigi@gmail.com>
2023-03-30 22:14:01 +0300

    Update task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,2 +1,25 @@
+#include "main.h"
 
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: string to be appended
+ * Return: pointer to the resulting string
+ */
 
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	/* Loop through dest to find its length */
+	for (j = 0; dest[j] != '\0'; j++)
+	{
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+
+	return (dest);
+}
