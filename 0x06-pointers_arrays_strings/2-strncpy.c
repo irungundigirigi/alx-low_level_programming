c0508546254636cec351e1f1686e943de6859d56
irungundigirigi <irungundigirigi@gmail.com>
2023-03-31 15:50:51 +0300

    Update task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,14 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 	int j;
 
 	for (j = 0; src[j] != '\0' && j < n; j++)
-	{	
+	{
 		dest[j] = src[j];
 	}
 
 	while(j < n)
 	{
 		dest[j] = '\0';
-	}	
+		j++;
+	}
 	return (dest);
 }
 
