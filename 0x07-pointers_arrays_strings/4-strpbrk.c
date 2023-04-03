75907d5183480fb4f3a403b31b901a7157e8adda
irungundigirigi <irungundigirigi@gmail.com>
2023-04-03 23:51:30 +0300

    Update task

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strpbrk - search string for any set set of bytes.
+ * @s: search in this string
+ * @accept: search for bytes from here
+ * Return: pointer to first occurence that matches one of the bytes.
+ * @return:Ends all loops
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while(s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if(s[i] == accept[j])
+			{
+				s = &s[i];
+				return (s);
+			}
+		}
+
+		i++;
+	}
+
+	return (NULL);
+}
