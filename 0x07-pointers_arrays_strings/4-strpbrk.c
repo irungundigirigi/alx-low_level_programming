bccf524cf22a870d3fbfdb52b2db986fc66ccfc2
irungundigirigi <irungundigirigi@gmail.com>
2023-04-03 23:54:25 +0300

    Update task

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,11 +12,11 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i = 0, j;
 
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if(s[i] == accept[j])
+			if (s[i] == accept[j])
 			{
 				s = &s[i];
 				return (s);
