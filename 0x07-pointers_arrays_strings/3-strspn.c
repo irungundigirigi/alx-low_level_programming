8090af22b172983936561c4950cfbdaf9c53b0a7
irungundigirigi <irungundigirigi@gmail.com>
2023-04-03 21:08:37 +0300

    Update task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -18,12 +18,12 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j] != '\0' && quit == 0; j++)
 		{
-			if(s[i] == accept[j])
+			if (s[i] == accept[j])
 			{
 				count++;
 				break;
 			}
-		}	if(s[i] != accept[j])
+		}	else if (s[i] != accept[j])
 				quit = 1;
 
 	}
