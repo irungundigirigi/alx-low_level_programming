8d4689251c4bddfe0116e35af38ff01e88718d09
irungundigirigi <irungundigirigi@gmail.com>
2023-04-03 21:04:41 +0300

    Update task

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the lenth of prefix substring
+ * @s: search in this string
+ * @accept: search for bytes from here
+ * Return: no of matching bytes in initial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int count = 0;
+	int quit = 0;
+
+	for (i = 0; s[i] != '\0' && quit == 0; i++)
+	{
+		for (j = 0; accept[j] != '\0' && quit == 0; j++)
+		{
+			if(s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+		}	if(s[i] != accept[j])
+				quit = 1;
+
+	}
+
+	return (count);
+}
