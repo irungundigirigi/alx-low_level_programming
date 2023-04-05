d038b3ba352d35e465a584641944de0bdf0c6ec2
irungundigirigi <irungundigirigi@gmail.com>
2023-04-05 22:58:40 +0300

    Update task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,29 +7,29 @@
 */
 
 
-int is_prime_number(int n) 
+int is_prime_number(int n)
 {
-    int divisor = 3;
+	int divisor = 3;
 
-    if (n % 2 == 0 || n < 2)
-        return (0);
-    if (n == 2)
-        return (1);
-
-    return (is_prime(n, divisor));    
+	if (n % 2 == 0 || n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (is_prime(n, divisor));
 }
 
 /**
 * is_prime - recursively tests if n is prime.
 * @n: check this number
-* divisor: divides n with divisor to check.
+* @divisor: divides n with divisor to check.
+* Return: recursion calling is_prime with divisor + 1
 */
 
 int is_prime(int n, int divisor)
 {
-    if (divisor > n/2)
-        return (1);
-    if (n % divisor == 0)
-        return (0); 
-    return(is_prime(n, divisor + 1));  
+	if (divisor > n / 2)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (is_prime(n, divisor + 1));
 }
