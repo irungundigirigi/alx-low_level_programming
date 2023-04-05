fd05d4f51133fed5d205b908f570260a5b0d4755
irungundigirigi <irungundigirigi@gmail.com>
2023-04-05 23:40:38 +0300

    Update task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * find_root - find square root of n, starting from 0
+ * @n: n
+ * @r: test if root
+ * Return: natural square root, or -1 if not natural root
+ */
+
+int find_root(int n, int r)
+{
+	if (r > n / 2)
+		return (-1);
+	if (r * r == n)
+		return (r);
+	return (find_root(n, r + 1));
+}
+
+/**
+ * _sqrt_recursion - find natural sqrt of n
+ * @n: n
+ * Return: natural sqrt / -1 if none
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (find_root(n, 0)):
+}
