7db90205e6be7734714aec8bd6336dfb7b2d9059
irungundigirigi <irungundigirigi@gmail.com>
2023-04-06 03:47:46 +0300

    Update

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - find factorial n
+ * @n: n
+ * Return:factorial of n, -1 if negative n
+ */
+
+int factorial(int n)
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+		return (n * factorial(n - 1));
+}	
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,12 @@
 
 int is_prime_number(int n);
 int is_prime(int n, int divisor);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
+int _putchar(char c);
 
 
 
