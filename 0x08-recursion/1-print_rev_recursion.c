3181829ecb9be917138897acaa07d2475a3f9963
irungundigirigi <irungundigirigi@gmail.com>
2023-04-06 03:51:35 +0300

    Update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,10 +5,10 @@
  * @s: string to be manipulated
  */
 
-void _print_rev_recursion(char *s) 
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
-}	
+}
