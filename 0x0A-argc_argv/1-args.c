094551b3a51ff2863553809b18d7127037e7f903
irungundigirigi <irungundigirigi@gmail.com>
2023-04-10 15:37:02 +0300

    Update task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the number of arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%d\n", argc;
+
+	return (0);
+}
