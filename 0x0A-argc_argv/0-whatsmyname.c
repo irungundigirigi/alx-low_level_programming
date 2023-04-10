7e0de777db649bedd0ef36300cd2957dde87b84c
irungundigirigi <irungundigirigi@gmail.com>
2023-04-10 15:29:46 +0300

    Update task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the executable file name followed by new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}	
