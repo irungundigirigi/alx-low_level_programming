44cd2170968a022019afa3e3e8343b68f43f27a3
irungundigirigi <irungundigirigi@gmail.com>
2023-04-13 18:25:08 +0300

    update task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,5 +2,6 @@
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+int *array_range(int min, int max);
 int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
