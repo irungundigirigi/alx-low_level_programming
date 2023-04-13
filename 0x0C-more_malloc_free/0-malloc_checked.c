1deda3ba0f33e15fa8e074103eda9d3b81a10eaf
irungundigirigi <irungundigirigi@gmail.com>
2023-04-13 14:54:21 +0300

    update task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory, if failed terminate with status 98
+ * @b: unsigned int memory size
+ * Return: pointer to allocated memory space
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}	
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_nconcat - concatenates string 1 with n bytes from string 2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes of s2 used
+ * Return: NULL if fail, else pointer to allocated memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	int length1, i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	length1 = (unsigned int)_strlen(s1);
+
+	p = malloc((length1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < length1; i++)
+		*(p + i) = s1[i];
+	
+	j = 0;
+
+	while ( i < (length1 + n)
+		*(p + i) = s2[j];
+		j++;
+}
+
+/** _strlen - find length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return(i);
+}
