3f545accd3c626cda32e27e32df66ff10948b18d
irungundigirigi <irungundigirigi@gmail.com>
2023-04-13 16:00:47 +0300

    update task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,11 +13,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
 	int length1, i, j;
+	int num = n;
 
-	if (n >= _strlen(s2))
-		n = _strlen(s2);
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
 
-	if (n <= 0)
+	if (num <= 0)
 		return (0);
 
 	if (s1 == NULL)
@@ -27,16 +28,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	length1 = (unsigned int)_strlen(s1);
 
-	p = malloc((length1 + n + 1) * sizeof(char));
+	p = malloc((length1 + num + 1) * sizeof(char));
 
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < length1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		*(p + i) = s1[i];
 
-
-	for (j = 0; j < n; j++)
+	for (j = 0; j < num; j++)
 		*(p + i + j) = s2[j];
 
 	*(p + i + j) = '\0';
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
-void *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
