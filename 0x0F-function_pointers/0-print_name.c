16d45955a73180c026e18340178851f1748762f5
irungundigirigi <irungundigirigi@gmail.com>
2023-04-19 22:35:36 +0300

    Update task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
  * @f: fn pointer, takes string argument
  */
 
-void print_name(char *name, void (*f)(char))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
 	f(name);
