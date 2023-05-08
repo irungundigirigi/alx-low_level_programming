b89e2dea1659de402dab9c2a13b3bb7bb578a0eb
irungundigirigi <irungundigirigi@gmail.com>
2023-05-08 16:57:53 +0300

    Add task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,14 +13,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
-	{	
+	{
 		/* find string length*/
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
@@ -33,5 +33,3 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
- 
- 
