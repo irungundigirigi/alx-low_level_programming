34444c536a1bdb68317aae7ba803011e9d3cf43c
irungundigirigi <irungundigirigi@gmail.com>
2023-05-08 13:24:38 +0300

    Update task 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file to read from
+ * @letters: size to read
+ * Return: size read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t size_r, size_w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	size_r = read(fd, buffer, letters);
+	if (size_r == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	size_w = write(STDOUT_FILENO, buffer, n_read);
+	if (size_w == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	close(fd);
+	return (size_r);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/* open */
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/* close */
+#include <unistd.h>
+
+/* dprintf */
+#include <stdio.h>
+
+/* malloc, free */
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
