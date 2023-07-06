76957d30d0715c58229d0a8027750e3d9ae29bf8
irungundigirigi <irungundigirigi@gmail.com>
2023-07-06 11:28:00 +0300

    add task 1.1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of no
+ * @number: unsigned long int
+ * Return: void
+ */
+
+void print_binary(unsigned long int number)
+{
+	if (number)
+	{
+		if (number >> 1)
+			print_binary(number >> 1);
+		_putchar((number & 1) + '0');
+	}
+	else 
+	{
+		_putchar('0');
+	}
+
+}
