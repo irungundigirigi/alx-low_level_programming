9b986822e7e54fe8fa34e66627a8441a5c8f69b3
irungundigirigi <irungundigirigi@gmail.com>
2023-07-06 10:22:50 +0300

    Add task 0.0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary no to unsigned int
+ * @binary: binary
+ * Return: unsigned int.
+ */
+
+unsigned int binary_to_uint(const char *binary)
+{
+	int len, position_value;
+	unsigned int j;
+
+	if (!i)
+		return (0);
+
+	for (len = 0; binary[len] != '\0'; len++)
+		;
+
+	for (len--, position_value = 1; len >= 0; len--, position_value *= 2)
+	{
+		if (binary[len] != '0' && binary[len] != '1')
+		{
+			return (0);
+		}
+
+		if (binary[len] & 1)
+		{
+			j += position_value;
+		}
+	}
+
+	return (j);
+}
