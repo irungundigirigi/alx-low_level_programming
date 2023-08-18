1af2234c2f9498e41f538a508eb9a2243b281992
irungundigirigi <irungundigirigi@gmail.com>
2023-08-18 11:54:44 +0300

    Push tasks - doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,69 +7,46 @@
  * @n: value to store in new node
  * Return: Address of new node, or NULL if failed
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n)
 {
-    unsigned int count = 1;
-    dlistint_t *tmp, *prev, *new_node;
+	unsigned int c;
+	dlistint_t *tmp, *prev, *new;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->n = n;
-    tmp = *h;
-
-    while (tmp && count < idx)
-    {
-        prev = tmp;
-        tmp = tmp->next;
-        count++;
-    }
-
-    if (idx == 0)
-    {
-        *h = new_node;
-        new_node->prev = NULL;
-        new_node->next = (tmp == NULL) ? NULL : tmp;
-        return (new_node);
-    }
-
-    if (idx == 1)
-    {
-        prev = *h;
-        tmp = ((*h)->next == NULL) ? NULL : (*h)->next;
-        new_node->prev = prev;
-        new_node->next = tmp;
-        prev->next = new_node;
-        if (tmp)
-            tmp->prev = new_node;
-        return (new_node);
-    }
-
-    if (idx == count && tmp == NULL)
-    {
-        if (prev != NULL)
-        {
-            new_node->prev = prev;
-            new_node->next = NULL;
-            prev->next = new_node;
-            return (new_node);
-        }
-        free(new_node);
-        return (NULL);
-    }
-    else if (idx != count && tmp == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-
-    prev = tmp;
-    tmp = tmp->next;
-    new_node->prev = prev;
-    new_node->next = tmp;
-    prev->next = new_node;
-    tmp->prev = new_node;
-    return (new_node);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	for (tmp = *h, c = 1; tmp && c < idx; c++, tmp = tmp->next)
+		prev = tmp;
+	if (idx == 0)
+	{
+		*h = new; new->prev = NULL;
+		new->next = (tmp == NULL) ? NULL : tmp;
+		return (new);
+	}
+	if (idx == 1)
+	{
+		prev = *h;
+		tmp = ((*h)->next == NULL) ? NULL : (*h)->next;
+		new->prev = prev; new->next = tmp; prev->next = new;
+		if (tmp)
+			tmp->prev = new;
+		return (new);
+	}
+	if (idx == c && tmp == NULL)
+	{
+		if (prev != NULL)
+		{
+			new->prev = prev; new->next = NULL;
+			prev->next = new; return (new);
+		}
+		free(new); return (NULL);
+	}
+	else if (idx != c && tmp == NULL)
+	{
+		free(new); return (NULL);
+	}
+	prev = tmp; tmp = tmp->next; new->prev = prev;
+	new->next = tmp; prev->next = new; tmp->prev = new;
+	return (new);
 }
-
